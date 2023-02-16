9cceda1e4014bb841aba2bb0141a9d51b3d71a17
daviaquino87 <davi26031@gmail.com>
2023-02-16 16:37:29 -0300

    feat: create entite user

---
+++ src/modules/users/models/User.ts
@@ -0,0 +1,51 @@
+import { Replace } from "helpers/Replace";
+import { randomUUID } from "node:crypto";
+
+interface IUserProps {
+  name: string;
+  email: string;
+  password: string;
+  created_at: Date;
+}
+
+export class User {
+  private _id: string;
+  private props: IUserProps;
+
+  constructor(props: Replace<IUserProps, { created_at?: Date }>, id?: string) {
+    this._id = id ?? randomUUID();
+    this.props = { ...props, created_at: props.created_at ?? new Date() };
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+
+  public set email(email: string) {
+    this.props.email = email;
+  }
+
+  public get email(): string {
+    return this.props.email;
+  }
+
+  public set password(password: string) {
+    this.props.password = password;
+  }
+
+  public get password(): string {
+    return this.props.password;
+  }
+
+  public get created_at(): Date {
+    return this.props.created_at;
+  }
+}
