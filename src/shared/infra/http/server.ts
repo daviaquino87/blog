8291298ef2b4768de06845ee1b6213a86ccab9b0
daviaquino87 <davi26031@gmail.com>
2023-02-16 16:13:25 -0300

    feat: validate type error

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+
 import { indexRouter } from "./routes/index.routes";
+import { verifyError } from "./middlewares/verifyError";
 
 const app = express();
 
 app.use(express.json());
 app.use(indexRouter);
 
+app.use(verifyError);
 app.listen(3000, () => {
   console.log("application running");
 });
